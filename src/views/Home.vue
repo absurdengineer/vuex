b0b8afb1c3c77e014d09ef6b74c6f4a574d5fba5
Md Dilshad Alam <mddalam1@gmail.com>
2021-11-07 13:28:51 +0530

    Implmnted Normal Counter.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="counter">{{ counter }}</div>
+    <div class="buttons">
+      <button @click="decreaseCounter">-</button>
+      <button @click="increaseCounter">+</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    },
+  },
 };
 </script>
+
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 0 10px;
+  width: 100px;
+}
+</style>
