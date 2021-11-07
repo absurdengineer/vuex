b9b5a9da570fac11af8551e458feed16c5f46ff0
Md Dilshad Alam <mddalam1@gmail.com>
2021-11-07 13:31:46 +0530

    Implmented Counter with VueX.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    counter: 0,
   },
   mutations: {
+    increaseCounter(state) {
+      state.counter++;
+    },
+    decreaseCounter(state) {
+      state.counter--;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
